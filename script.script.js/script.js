f42e8bc3d2107ba03c6e5d0ddcb5b833a8539d1a
neeltara.secret.G <neeltarasgroup@gmail.com>
2024-09-29 10:00:18 +0530

    well my first project end from here i,m starting second project

--- script.script.js/script.js
+++ script.script.js/script.js
@@ -1,13 +1,9 @@
-function showSection(sectionId) {
-    // Hide all sections
-    var sections = document.querySelectorAll('.content-section');
-    sections.forEach(function(section) {
-        section.classList.add('hidden');
-    });
-
-    // Show the selected section
-    var sectionToShow = document.getElementById(sectionId);
-    sectionToShow.classList.remove('hidden');
-}
-
-// document. querySelectorAll('style,link[rel="stylesheet"]') .forEach (e=> e.remove());
+// script.js
+document.getElementById('menu-toggle').addEventListener('click', function() {
+    var menu = document.getElementById('menu');
+    if (menu.classList.contains('hidden')) {
+        menu.classList.remove('hidden');
+    } else {
+        menu.classList.add('hidden');
+    }
+});
